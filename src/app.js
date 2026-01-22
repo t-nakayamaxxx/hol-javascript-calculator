f5c4516652c5909538e0a53836d9c1a987807b77
t-nakayamaxxx <naka@mituna.jp>
2026-01-22 16:49:13 +0900

    commit

--- src/app.js
+++ src/app.js
@@ -17,12 +17,17 @@ function divide(a, b) {
     return a / b;
 }
 
+function square(a){
+    return multiply(a, a);
+}
+
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-    module.exports = { add, subtract, multiply, divide };
+    module.exports = { add, subtract, multiply, divide, square };
 } else {
     // ブラウザ環境でのエクスポート
     window.add = add;
     window.subtract = subtract;
     window.multiply = multiply;
     window.divide = divide;
+    window.square = square;
 }
